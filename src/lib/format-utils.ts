3229e53d92547bc36d275ebf357dbb553a612036
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-11-28 18:45:00 -0500

    Add number and currency formatting utilities

---
+++ src/lib/format-utils.ts
@@ -0,0 +1,18 @@
+export function formatNumber(num: number): string {
+  return new Intl.NumberFormat().format(num);
+}
+
+export function formatCurrency(amount: number, currency: string = 'USD'): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+  }).format(amount);
+}
+
+export function formatPercentage(value: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'percent',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  }).format(value);
+}
