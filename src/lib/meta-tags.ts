f80be14803094f61650f68cf5acbc79e963b0625
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-03 14:30:00 -0500

    Add meta tags configuration

---
+++ src/lib/meta-tags.ts
@@ -0,0 +1,8 @@
+export const metaTags = {
+  charset: "utf-8",
+  viewport: "width=device-width, initial-scale=1",
+  robots: "index, follow",
+  author: "Kevin Valencia",
+  creator: "Kevin Valencia",
+  publisher: "Kevin Valencia",
+};
