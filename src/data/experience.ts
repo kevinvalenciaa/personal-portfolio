271c688035348e7a44f8d675306cc83496b59926
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-07 17:45:00 -0500

    Add work experience data

---
+++ src/data/experience.ts
@@ -0,0 +1,4 @@
+export const workExperience = [
+  { company: "Tech Solutions Inc", role: "Frontend Developer", duration: "2023-Present", description: "Led development of React applications" },
+  { company: "Digital Agency", role: "Web Developer", duration: "2022-2023", description: "Built responsive websites and web apps" }
+];
