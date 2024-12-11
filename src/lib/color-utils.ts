edcc167f230f9dae61d1951cd5e70278c07949ea
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-11 18:00:00 -0500

    Add portfolio feature set 5

---
+++ src/lib/color-utils.ts
@@ -0,0 +1,14 @@
+export function generateColorPalette(baseColor: string) {
+  return {
+    50: `${baseColor}-50`,
+    100: `${baseColor}-100`,
+    200: `${baseColor}-200`,
+    300: `${baseColor}-300`,
+    400: `${baseColor}-400`,
+    500: `${baseColor}-500`,
+    600: `${baseColor}-600`,
+    700: `${baseColor}-700`,
+    800: `${baseColor}-800`,
+    900: `${baseColor}-900`,
+  };
+}
