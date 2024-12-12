40efac21ccbad9b365c0552e96d6472454dbbdc4
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-12 00:00:00 -0500

    Add portfolio feature set 8

---
+++ src/data/social-proof.ts
@@ -0,0 +1,8 @@
+export const socialProof = {
+  githubStars: 125,
+  linkedinConnections: 500,
+  projectsCompleted: 30,
+  clientSatisfaction: 98,
+  responseTime: "< 2 hours",
+  availability: "Full-time freelance"
+};
