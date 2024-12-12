e002831bb5b168e335f30bac46197eb68751dc8f
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-11 22:00:00 -0500

    Add portfolio feature set 7

---
+++ src/lib/use-intersection-observer.ts
@@ -0,0 +1,17 @@
+export function useIntersectionObserver(ref: React.RefObject<Element>, options?: IntersectionObserverInit) {
+  const [isIntersecting, setIsIntersecting] = React.useState(false);
+
+  React.useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    }, options);
+
+    observer.observe(element);
+    return () => observer.disconnect();
+  }, [ref, options]);
+
+  return isIntersecting;
+}
