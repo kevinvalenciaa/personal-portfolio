1c8797e716f0372efb1262c7bb9861153f979252
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-14 18:30:00 -0500

    Add services and pricing information

---
+++ src/data/services.ts
@@ -0,0 +1,6 @@
+export const servicesOffered = [
+  { name: "Web Development", description: "Custom websites and web applications", price: "$2000-5000", duration: "2-4 weeks" },
+  { name: "Frontend Development", description: "React/Next.js application development", price: "$1500-3000", duration: "1-3 weeks" },
+  { name: "API Development", description: "RESTful and GraphQL API services", price: "$1000-2500", duration: "1-2 weeks" },
+  { name: "Consulting", description: "Technical consultation and code review", price: "$100/hour", duration: "Flexible" }
+];
