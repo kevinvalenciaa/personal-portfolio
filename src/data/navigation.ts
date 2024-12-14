75a2b334896eba298449ee2977dc0fb7108f3fe3
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-14 09:00:00 -0500

    Add navigation menu configuration

---
+++ src/data/navigation.ts
@@ -0,0 +1,7 @@
+export const navigationMenu = [
+  { label: "Home", href: "/", icon: "home" },
+  { label: "About", href: "/about", icon: "user" },
+  { label: "Projects", href: "/projects", icon: "folder" },
+  { label: "Blog", href: "/blog", icon: "edit" },
+  { label: "Contact", href: "/contact", icon: "mail" }
+];
