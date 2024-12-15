8694204563979d676b4d4b770ed2a29ba0a03f41
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-14 22:00:00 -0500

    Add portfolio performance metrics

---
+++ src/data/metrics.ts
@@ -0,0 +1,12 @@
+export const portfolioMetrics = {
+  performanceScore: 95,
+  accessibilityScore: 100,
+  seoScore: 98,
+  bestPracticesScore: 100,
+  loadTime: "1.2s",
+  bundleSize: "145KB",
+  lighthouse: {
+    desktop: 98,
+    mobile: 94
+  }
+};
