702594ccce5de01b91ddc670e8d7857b372f41d6
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-17 16:30:00 -0500

    Add contact methods configuration

---
+++ src/data/contact.ts
@@ -0,0 +1,4 @@
+export const contactMethods = [
+  { type: "email", value: "kevinangelo.valencia10@gmail.com", primary: true },
+  { type: "linkedin", value: "https://linkedin.com/in/kevinvalencia", primary: false }
+];
