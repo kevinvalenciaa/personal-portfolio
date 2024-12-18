0063f8ee2218a48ab5d5def57d28ba8d4ec1bb18
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-18 09:30:00 -0500

    Add education background data

---
+++ src/data/education.ts
@@ -0,0 +1,4 @@
+export const education = [
+  { degree: "Bachelor of Computer Science", school: "State University", year: "2022", gpa: "3.8" },
+  { degree: "Frontend Development Bootcamp", school: "Code Academy", year: "2021", duration: "6 months" }
+];
