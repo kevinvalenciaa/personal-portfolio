bfe583918d23bb6b9dd310fad07143f8bba5b472
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-12-18 17:30:00 -0500

    Add volunteer work experience

---
+++ src/data/volunteering.ts
@@ -0,0 +1,4 @@
+export const volunteering = [
+  { organization: "Code for Good", role: "Volunteer Developer", duration: "2023-Present" },
+  { organization: "Local Library", role: "Tech Mentor", duration: "2022-2023" }
+];
