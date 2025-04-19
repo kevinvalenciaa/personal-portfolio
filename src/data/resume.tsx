0419b67ddf30b923d55f45e3ad93b614469c229a
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2025-04-19 00:09:34 -0400

    updated qsao project

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -228,7 +228,7 @@ export const DATA = {
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "/qsao.png",
+      image: "/QSAO.png",
     },
     {
       title: "NumNum",
